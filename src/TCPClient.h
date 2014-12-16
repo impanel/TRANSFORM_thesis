10e86477a1dd21cbf12779c77d7617bde46e6d6b
Philipp <phil_s@mit.edu>
2014-12-16 15:01:11 -0500

    resolved missing votable error

--- src/TCPClient.h
+++ src/TCPClient.h
@@ -31,16 +31,6 @@ public:
     void draw();
     
     void keyPressed(int key);
-    void keyReleased(int key);
-    void mouseMoved(int x, int y );
-    void mouseDragged(int x, int y, int button);
-    void mousePressed(int x, int y, int button);
-    void mouseReleased(int x, int y, int button);
-    void windowResized(int w, int h);
-    void dragEvent(ofDragInfo dragInfo);
-    void gotMessage(ofMessage msg);
-    
-    
     void drawPinsDebug(unsigned char * _theColors);
     void drawPins(unsigned char * _theColors);
     void renderShape();
